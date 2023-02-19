35473cb437dcd4b92b57d0ea893f1abbf5e7842b
nomfundoS23 <nomfundo21ss@gmail.com>
2023-02-19 06:47:52 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  main - this is the function to printf
+ *
+ *  Return: 0 when  successful
+ */
+
+int main(void)
+{
+	printf("with proper gramar, but the outcome is a piece of art,\n");
+	return (0);
+}
