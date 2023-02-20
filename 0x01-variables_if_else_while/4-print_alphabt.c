5e1470eccce3c06fb97d97db40ddb9e2e4685e7d
nomfundoS23 <nomfundo21ss@gmail.com>
2023-02-20 14:23:48 -0800

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,17 +9,13 @@
 
 int main(void)
 
-{ 
-	char ch = 'a';
+{
 
-	while (ch <= 'z')
-	{
-		if ((ch != 'e') && (ch != 'q'))
-		{
-			putchar(ch);
-		}
-		ch++;
-	}
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
 	putchar('\n');
 	return (0);
 }
