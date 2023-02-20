425873e3fa0b6a41355f9e9fc594ed1416f9148b
nomfundoS23 <nomfundo21ss@gmail.com>
2023-02-20 15:06:46 -0800

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,16 +8,18 @@
 
 int main(void)
 {
-	int i; j;
+	int i;
+	int j;
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; 0; j < 100; j++)
+		for (j = 0; j < 100; j++)
 		{
 			if (i < j)
 			{
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
+				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
@@ -28,6 +30,6 @@ int main(void)
 			}
 		}
 	}
-	putchar('n\')
+	putchar('\n');
 	return (0);
 }
