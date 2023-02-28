7d6ba80e5b3f5c844f6ef6558e42a5f606128a38
nomfundoS23 <nomfundo21ss@gmail.com>
2023-02-28 12:55:44 -0800

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,8 +9,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	if (dest == NULL)
-		return (NULL);
+	if (dest == NULL) return (NULL);
 	for (i = 0 ; src[i] != '\0' ; i++)
 		dest[i] = src[i];
 	return (dest);
